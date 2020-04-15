698016f2617f7404d8ead109be8bec1e65006ec1
aashishdahal1 <aashdahal@gmail.com>
2020-04-15 19:52:01 +0545

    remove comments

--- server.go
+++ server.go
@@ -13,13 +13,6 @@ var (
 
 func main() {
 	server := gin.Default()
-	// server.GET("/test", func(ctx *gin.Context) {
-	// 	ctx.JSON(200, gin.H{"message": "Hello fuckers"})
-	// })
-	// server.GET("/", func(ctx *gin.Context) {
-	// 	ctx.JSON(200, gin.H{"message": "Welcome to go"})
-	// })
-
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
